ecadf4e234a3f85da1b2f39df3b517b7afb48821
pampero <carlos.vazquez@outlook.com>
2013-04-24 02:08:32 -0300

    Agregado log4net

---
+++ src/Common/Base/CustomUserSession.cs
@@ -0,0 +1,11 @@
+﻿using ServiceStack.ServiceInterface.Auth;
+
+namespace Model.Model.entities
+{
+    //A customizeable typed UserSession that can be extended with your own properties
+    //To access ServiceStack's Session, Cache, etc from MVC Controllers inherit from ControllerBase<CustomUserSession>
+    public class CustomUserSession : AuthUserSession
+    {
+        public string CustomProperty { get; set; }
+    }
+}
