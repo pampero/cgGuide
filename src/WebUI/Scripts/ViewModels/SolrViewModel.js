f666d7798866eb3de9652ea02697d3c5d2f26c95
carlos vazquez <carlos.daniel.vazquez@gmail.com>
2013-05-26 22:58:59 -0300

    DEMO SOLR PARTE 2
    Falta KO.
    Implementado Drill-Down pero hardcodeado.
    Parallels no funciona para eliminar el último elemento.

--- src/WebUI/Scripts/ViewModels/SolrViewModel.js
+++ src/WebUI/Scripts/ViewModels/SolrViewModel.js
@@ -38,7 +38,9 @@ function ParallelCollection(key, parallels) {
 function SellersViewModel() {
     var self = this;
     var searchParameters;
-    
+    self.sellers = ko.observableArray([]);
+    self.parallels = new Array();
+
     $.getJSON("http://localhost:1586/Search/GetAllSellers",  JSON.stringify( {  } ), function (result) {
         self.sellers.removeAll();
         
@@ -51,27 +53,37 @@ function SellersViewModel() {
     });
 
     // PENDIENTE: HACER EL OBJETO SEARCHPARAMETERS PARA MANEJARLO ENTERAMENTE CON KNOCKOUT.
-    self.clickCheckBox = function () {
+    self.clickCheckBox = function (input) {
+        //alert(self);
         
-        var keyValuePair1 = new KeyValuePair("1", "Faceta 1");
-        var keyValuePair2 = new KeyValuePair("2", "Faceta 2");
+        var keyValuePair1 = new KeyValuePair(input.id.split("|")[0], input.id.split("|")[1]);
+        //var keyValuePair2 = new KeyValuePair("2", "Faceta 2");
 
-        var facets = [keyValuePair1, keyValuePair2];
-        var parallels = [keyValuePair1, keyValuePair2];
+        //var facets = [keyValuePair1, keyValuePair2];
+        //var parallels = [keyValuePair1];
         
-        var facetCollection = new FacetCollection("Categoria1", facets);
+        //var facetCollection = new FacetCollection("Categoria1", facets);
         
-        var parallelCollection = new ParallelCollection("Categoria 2", parallels);
+        //var parallelCollection = new ParallelCollection("Categoria 2", parallels);
         
         //  searchParameters = new SearchParameters("FreeSearch", facetCollection, parallelCollection);
-        searchParameters = new SearchParameters("FreeSearch", facets, parallels);
+        //searchParameters = new SearchParameters("FreeSearch", facets, parallels);
+       // searchParameters = new SearchParameters();
+
+        if (input.checked) {
+            self.parallels.push(keyValuePair1);
+        }
+        else {
+            var index = self.parallels.indexOf(keyValuePair1.Key);
+            self.parallels.splice(index, 1);
+        }
 
         $.ajax({
             type: 'POST',
             url: "http://localhost:1586/Search/GetAllSellers",
             contentType: "application/json; charset=utf-8",
             traditional: true,
-            data: JSON.stringify({ searchParametersDto: searchParameters }),
+            data: JSON.stringify({ parallelsDto: self.parallels }),
             success: function (result) {
                 self.sellers.removeAll();
                 $.map(result.sellers, function (seller) {
@@ -86,9 +98,9 @@ function SellersViewModel() {
         });
     };
 
-    self.sellers = ko.observableArray([]);
+    
    
 }
 
-
-ko.applyBindings(new SellersViewModel());
+var sellerViewModel = new SellersViewModel();
+ko.applyBindings(sellerViewModel);
